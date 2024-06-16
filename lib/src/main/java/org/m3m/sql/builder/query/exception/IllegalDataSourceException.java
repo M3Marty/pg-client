990b52adaea51f4baea0a8e47ff65be0f31b280e
M3Marty <maxoff.soul@gmail.com>
2024-06-16 13:18:11 +0300

    Remove exceptions

--- lib/src/main/java/org/m3m/sql/builder/query/exception/IllegalDataSourceException.java
+++
@@ -1,8 +0,0 @@
-package org.m3m.sql.builder.query.exception;
-
-public class IllegalDataSourceException extends SqlException {
-
-	public IllegalDataSourceException(String message) {
-		super(message);
-	}
-}
